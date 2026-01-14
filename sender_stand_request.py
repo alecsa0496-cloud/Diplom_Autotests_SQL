cfddf2da4d335eebdaf6405afd7e3cebc0c0acdb
AleksM <alecsa0496@gmail.com>
2026-01-14 16:45:25 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,13 +1,21 @@
 import configuration
 import requests
 
-# Создание заказа
-def create_order(body):
-    return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER,
-                         json=body)
 
-# Получение заказа по номеру трекера
+def create_order(body):
+    """Создание нового заказа."""
+    return requests.post(
+        configuration.URL_SERVICE + configuration.CREATE_ORDER,
+        json=body
+    )
+
+
 def get_order(track_number):
-    get_order_url = f"{configuration.URL_SERVICE}{configuration.FETCH_ORDER_TRACK}?t={track_number}"
+    """Получение информации о заказе по номеру трека."""
+    get_order_url = (
+        f"{configuration.URL_SERVICE}"
+        f"{configuration.FETCH_ORDER_TRACK}"
+        f"?t={track_number}"
+    )
     response = requests.get(get_order_url)
     return response
